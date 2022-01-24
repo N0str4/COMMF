2f9176b6dde849686eeccaaaa6ad9353cdcd6521
N0str4 <aymerickvalois@yahoo.fr>
2022-01-24 09:57:04 +0100

    Patch Note
    Logs séparé dans le menu.
    Logs Recherche/Formation/Prérequis.
    Possibilité de Voir si le logs est un ajout ou un delete

---
+++ Login/administration/fonctions/supprimerlogsajoutprerequis.php
@@ -0,0 +1,21 @@
+<?php
+
+include '../config/config.php';
+
+
+try{
+    //On insère les données reçues
+
+
+    $delete = $bdd->prepare("
+    DELETE FROM logajoutpre");
+    $delete->execute();
+
+  }
+  catch(PDOException $e){
+    echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
+  }
+
+?><script type="text/javascript">
+window.location.replace("http://intradef.vikatchev.com/Login/administration/logsajout.php");
+</script>
