815cea069e06126baad53281748f623c415f9e8e
N0str4 <aymerickvalois@yahoo.fr>
2022-02-07 07:59:32 +0100

    Ajout d'un icon DashBoard Message

--- Login/administration/index.php
+++ Login/administration/index.php
@@ -171,7 +171,8 @@ include 'config/menu.php';
       <? if($messageexiste >0){?>
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 <i class="bi bi-exclamation-triangle me-1"></i>
-                <?php echo '<b>'.$messageError.'</b>'?>
+                <?php echo '<b> Message du jour : '.$messageError.'</b>    '?>
+                <i class="bi bi-exclamation-triangle me-1"></i>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
       <div class="row">
