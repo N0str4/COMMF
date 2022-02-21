b2f531a2c284dd593e0c0b97f03e64f7aeebe5fd
N0str4 <N0str4@yahoo.fr>
2022-02-21 16:11:34 +0100

    Release page personnel vivier + patch tableau regroupement

--- Login/administration/regroupement.php
+++ Login/administration/regroupement.php
@@ -184,3 +184,26 @@ $numPrerequis=$numPrerequisFormation;
            // echo '<br>YES<br>';
         }
     }*/
+?>
+    </div>
+    </div>
+
+  </div>
+</div>
+</section>
+
+</main><!-- End #main -->
+</main><!-- End #main -->
+<!-- Vendor JS Files -->
+<!-- Vendor JS Files -->
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/chart.js/chart.min.js"></script>
+<script src="assets/vendor/echarts/echarts.min.js"></script>
+<script src="assets/vendor/quill/quill.min.js"></script>
+<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
